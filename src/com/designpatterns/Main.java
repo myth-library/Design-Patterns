0420ca1be38f90e13ef631e1d8214182ca811c0d
Myth <ashenjayarathnedev@gmail.com>
2024-05-11 05:51:47 +0530

    The problem

--- src/com/designpatterns/Main.java
+++ src/com/designpatterns/Main.java
@@ -1,7 +1,14 @@
 package com.designpatterns;
 
+import com.designpatterns.singleton.ConfigManager;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		ConfigManager manager = new ConfigManager();
+		manager.set("name", "Ashen");
+//		System.out.println(manager.get("name"));
+		
+		ConfigManager other = new ConfigManager();
+		System.out.println(other.get("name"));
 	}
 }
---
+++ src/com/designpatterns/singleton/ConfigManager.java
@@ -0,0 +1,17 @@
+package com.designpatterns.singleton;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConfigManager {
+	private Map<String, Object> settings = new HashMap<>();
+	
+	public void set(String key, Object value) {
+		settings.put(key, value);
+	}
+	
+	public Object get(String key) {
+		return settings.get(key);
+	}
+	
+}
