13905df0dfa301ee078b25a518ddd949f76d45c6
Myth <ashenjayarathnedev@gmail.com>
2024-05-11 06:06:34 +0530

    Solution (singleton)

--- src/com/designpatterns/Main.java
+++ src/com/designpatterns/Main.java
@@ -4,11 +4,11 @@ import com.designpatterns.singleton.ConfigManager;
 
 public class Main {
 	public static void main(String[] args) {
-		ConfigManager manager = new ConfigManager();
+		ConfigManager manager = ConfigManager.getInstance();
 		manager.set("name", "Ashen");
 //		System.out.println(manager.get("name"));
 		
-		ConfigManager other = new ConfigManager();
+		ConfigManager other = ConfigManager.getInstance();
 		System.out.println(other.get("name"));
 	}
 }
--- src/com/designpatterns/singleton/ConfigManager.java
+++ src/com/designpatterns/singleton/ConfigManager.java
@@ -5,6 +5,13 @@ import java.util.Map;
 
 public class ConfigManager {
 	private Map<String, Object> settings = new HashMap<>();
+	private static ConfigManager instance = new ConfigManager();
+	
+	private ConfigManager() {}
+	
+	public static ConfigManager getInstance() {
+		return instance;
+	}
 	
 	public void set(String key, Object value) {
 		settings.put(key, value);
